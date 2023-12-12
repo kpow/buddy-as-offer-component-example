688f9a97ee0c24d23e65c8679811ea686429ab6d
Kevin Power <kevinpower@kpowers-air-m1.lan>
2023-12-12 09:15:34 -0500

    testing new prefill

--- app/page.tsx
+++ app/page.tsx
@@ -6,39 +6,38 @@ import BuddyOfferElement, { useConfig } from '@buddy-technology/offer-component'
 // example prefill data, makem sure the start date is in the future :)
 const demodata = {
   policy: {
-    headlinePrefill: "Prefill view headline for larger screens",
-    headlinePrefillMobile: "Prefill view Mobile headline",
-    headlineIntro: "Headline for larger screens",
-    headlineIntroMobile: "Mobile headline",
-    headlineCoverages:
-      "We have options for you to find the right renters coverage for your needs.",
+    headlinePrefill: 'Prefill view headline for larger screens',
+    headlinePrefillMobile: 'Prefill view Mobile headline',
+    headlineIntro: 'Headline for larger screens',
+    headlineIntroMobile: 'Mobile headline',
+    headlineCoverages: 'We have options for you to find the right renters coverage for your needs.',
     headlineCoveragesMobile: "We've got options for you",
-    startDate: "12/30/2023",
+    startDate: '12/30/2023',
     renters: {
       address: {
-        line1: "8226 S Langley Ave",
-        line2: "",
-        postalCode: "60619",
-        state: "IL",
-        city: "Chicago"
+        line1: '8226 S Langley Ave',
+        line2: '',
+        postalCode: '60619',
+        state: 'IL',
+        city: 'Chicago'
       }
     }
   },
   customer: {
-    firstName: "Dan",
-    lastName: "Joyce",
-    dob: "",
-    email: "DandLJoyce@att.net",
-    phone: "+12242130386",
+    firstName: 'Jennifer',
+    lastName: 'James',
+    dob: '04/04/1990',
+    email: 'enterYourEmail@gmail.com',
+    phone: '+18049186025',
     address: {
-      line1: "3516 Lincoln Way",
-      line2: "Apt. #48",
-      city: "Ames",
-      state: "IA",
-      postalCode: "50014"
-    }
+      line1: '123 E Main St',
+      line2: '',
+      city: 'West Chicago',
+      state: 'IL',
+      postalCode: '60185'
+    },
   }
-};
+};;
 
 
 
