8a686c1860531ab5499b63a402ec967bb9b03e1e
cshepscorp <sheppard.christy@gmail.com>
2024-02-02 16:11:30 -0500

    feat: basics of pull from params to load into dataset

--- src/pages/allstateCompanion.tsx
+++ src/pages/allstateCompanion.tsx
@@ -1,38 +1,71 @@
 'use client'
 // import the Buddy Offer Element and the useConfig hook
 import BuddyOfferElement, { useConfig } from '@buddy-technology/offer-component';
-
+import { useRouter } from 'next/router'
 // use query string with incoming data to construct this object
-const demodata = {
-  policy: {
-    startDate: '02/26/2024',
-    renters: {
-      address: {
-        line1: '3505 Barkley Rd',
-        line2: '',
-        postalCode: '29154',
-        state: 'SC',
-        city: 'Sumter'
-      }
-    }
-  },
-  customer: {
-    firstName: 'Jennifer',
-    lastName: 'James',
-    dob: '04/04/1990',
-    email: 'enterYourEmail@gmail.com',
-    phone: '+18049186025',
-    address: {
-      line1: '123 E Main St',
-      line2: '',
-      city: 'West Chicago',
-      state: 'IL',
-      postalCode: '60185'
-    },
-  }
-};
 
 function OfferElement() {
+  const router = useRouter()
+  const { firstName, lastName } = router.query
+  //console.log('firstName', firstName);
+  const demodata = {
+    policy: {
+      startDate: '04/04/2024',
+      renters: {
+        address: {
+          line1: '3505 Barkley Rd',
+          line2: '',
+          postalCode: '29154',
+          state: 'SC',
+          city: 'Sumter'
+        }
+      }
+    },
+    customer: {
+      firstName: firstName,
+      lastName: lastName,
+      dob: '04/04/1990',
+      email: 'enterYourEmail@gmail.com',
+      phone: '+18049186025',
+      address: {
+        line1: '123 E Main St',
+        line2: '',
+        city: 'West Chicago',
+        state: 'IL',
+        postalCode: '60185'
+      },
+    }
+  };
+  
+  const demodataGood = {
+    policy: {
+      startDate: '02/26/2024',
+      renters: {
+        address: {
+          line1: '3505 Barkley Rd',
+          line2: '',
+          postalCode: '29154',
+          state: 'SC',
+          city: 'Sumter'
+        }
+      }
+    },
+    customer: {
+      firstName: 'Jennifer',
+      lastName: 'James',
+      dob: '04/04/1990',
+      email: 'enterYourEmail@gmail.com',
+      phone: '+18049186025',
+      address: {
+        line1: '123 E Main St',
+        line2: '',
+        city: 'West Chicago',
+        state: 'IL',
+        postalCode: '60185'
+      },
+    }
+  };
+  
   // useConfig hook to retrieve configuration options
   const { config, isLoading } = useConfig(
     "https://staging.embed.buddy.insure/allstate/renters/allstate-renters-prefill-config-react.js"
@@ -45,13 +78,13 @@ function OfferElement() {
   return (
     <div className="App w-full">
       {/* The div the offer element loads into */}
-      <h1>staging</h1>
+      <h1>Allstate Companion ION loads here </h1>
       <div id="buddy_offer" />
 
       <BuddyOfferElement
-        ion="ALLSTATE_RENTERS_PREFILL"
+        ion="ALLSTATE_RENTERS_PREFILL_COMPANION"
         partnerID="p-buddytest"
-        stage="STAGING"
+        stage="TESTING"
         data={demodata}
         theme={config.themeBase}
         onUserEvent={config.userEvents}
