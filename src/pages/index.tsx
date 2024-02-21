841d72eb538ffdd5ff390eb767866fa7369ddd6a
cshepscorp <sheppard.christy@gmail.com>
2024-02-21 14:46:22 -0500

    chore: updated True/False to On/Off for companionMode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,8 +47,8 @@ const ParentComponent = () => {
             <br />
             <select onChange={handleCompanionModeChange} value={companionMode} className="px-4 py-2 my-2 bg-white border border-gray-300 rounded-lg shadow-sm focus:outline-none pr-10">
               <option value="">Companion mode</option>
-              <option value="True">True</option>
-              <option value="False">False</option>
+              <option value="True">On</option>
+              <option value="False">Off</option>
             </select>
           {/* <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
             <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"/></svg>
